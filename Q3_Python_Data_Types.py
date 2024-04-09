90f7bdb94f58bbae23f6710f1b55ffaf3f1da783
Ajani Brown <ajani1brown.gmail.com>
2024-04-09 17:07:49 -0400

    first commit

---
+++ Q3_Python_Data_Types.py
@@ -0,0 +1,9 @@
+variable_a = "Hello, World!" # This data type is a string
+variable_b = 23 # This data type is an integer
+variable_c = 3.14 # This data type is a Float
+variable_d = True # This data type is a Boolean
+
+print(type(variable_a))
+print(type(variable_b))
+print(type(variable_c))
+print(type(variable_d))
---
+++ Q4_Arithmetic_Operations.py
@@ -0,0 +1,19 @@
+#Task 1: Grocery Store Math
+
+cereal, milk, eggs = 5.99, 3.99, 4.99
+
+total_cost = cereal + milk + eggs
+
+print(total_cost)
+
+#Task 2: Bank Interest
+
+int_balance = 35000
+four_percent_rate =0.04
+num_of_years = 1
+interest = int_balance*four_percent_rate*num_of_years
+print(interest)
+
+total_balance = interest*num_of_years+int_balance
+print(total_balance)
+
